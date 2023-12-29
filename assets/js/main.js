7e7756687251b422f23548bb9e70891d8517b7c4
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-12-29 12:05:41 -0300

    Separa o consumo da API da manipulação de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+// const offset = 0;
+// const limit = 10;
+// const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon) {
-  return `
+    return `
         <li class="pokemon">
         <span class="number">#001</span>
         <span class="name">${pokemon.name}</span>
@@ -21,13 +21,13 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById("pokemonList");
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((pokemons) => {
+pokeApi.getPokemons().then((pokemons) => {
+    const listItems = [];
+
     for (let i = 0; i < pokemons.length; i++) {
-      const pokemon = pokemons[i];
-      pokemonList.innerHTML += convertPokemonToLi(pokemon);
+        const pokemon = pokemons[i];
+        listItems.push(convertPokemonToLi(pokemon));
     }
-  })
-  .catch((error) => console.log(error));
+
+    console.log(listItems);
+});
