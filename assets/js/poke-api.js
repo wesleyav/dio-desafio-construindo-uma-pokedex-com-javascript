eb93884e7b0b29ab4174132617df341767d7ab20
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2023-12-29 13:54:34 -0300

    Refatora código

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -8,3 +8,12 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
     .then((jsonBody) => jsonBody.results)
     .catch((error) => console.error);
 };
+
+Promise.all((
+  fetch('https://pokeapi.co/api/v2/pokemon/1'),
+  fetch('https://pokeapi.co/api/v2/pokemon/2'),
+  fetch('https://pokeapi.co/api/v2/pokemon/3'),
+  fetch('https://pokeapi.co/api/v2/pokemon/4')
+)).then((results) => {
+  console.log(results);
+});
